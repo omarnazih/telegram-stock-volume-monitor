cdb3d813dc667bacfd1cdd18b32a65d3ef6132ce
omarnazih <omarnazihcs@gmail.com>
2023-05-20 23:37:47 +0300

    update: update scraper price filter

--- stocks_scraper.py
+++ stocks_scraper.py
@@ -7,7 +7,7 @@ CSV_FILE = cfg.CSV_FILENAME
 
 def generate_csv():
     # Filters From website finviz : https://finviz.com/screener.ashx?v=111&f=cap_micro,sh_float_u100,sh_price_u20
-    filters = ['cap_micro', 'sh_float_u100','sh_price_u20']  # Shows companies Thas has cap micro,float under 100mil and price under20$
+    filters = ['cap_micro', 'sh_float_u100','sh_price_u10']  # Shows companies Thas has cap micro,float under 100mil and price under20$
 
     # Get the performance table and sort it by price ascending
     stock_list = Screener(filters=filters, table='Performance', order='price') 
@@ -17,4 +17,5 @@ def generate_csv():
     
 
 if __name__ == "__main__":
+    # !todo Create a cron job to run the scraper every day
     generate_csv()
